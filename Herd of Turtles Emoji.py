222ee7961f6662288baf1dfd4ef691d6f6174253
100168747 <whatsitsname123@gmail.com>
2019-09-10 15:51:33 -0500

    I got started

---
+++ Herd of Turtles Emoji.py
@@ -0,0 +1,14 @@
+import turtle
+
+Bob = turtle.Turtle()
+Bobe = turtle.Turtle()
+bawb = turtle.Turtle()
+bobb = turtle.Turtle()
+
+
+Bob.circle(100)
+Bobe.forward(200)
+bawb.backward(100)
+bobb.backward(200)
+
+turtle.exitonclick()
