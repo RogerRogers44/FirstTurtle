9e3975e8e10d51deb6815839ef4f6efca51d6c53
100168747 <whatsitsname123@gmail.com>
2019-09-11 15:52:31 -0500

    I made some progress

--- Herd of Turtles Emoji.py
+++ Herd of Turtles Emoji.py
@@ -4,11 +4,64 @@ Bob = turtle.Turtle()
 Bobe = turtle.Turtle()
 bawb = turtle.Turtle()
 bobb = turtle.Turtle()
+bbob = turtle.Turtle()
+bowb = turtle.Turtle()
 
+Bob.color("yellow")
+Bobe.color("black")
+bawb.color("red")
+bobb.color("purple")
+bbob.color("red")
+bowb.color("white")
 
-Bob.circle(100)
+# Face fill
+Bob.penup()
+Bob.right(90)
+Bob.forward(200)
+Bob.left(90)
+Bob.pendown()
+Bob.begin_fill()
+Bob.circle(250)
+Bob.end_fill()
+
+# Outline
+Bobe.penup()
+Bobe.right(90)
 Bobe.forward(200)
-bawb.backward(100)
+Bobe.left(90)
+Bobe.pendown()
+Bobe.circle(250)
+# Mouth
+bawb.penup()
+bawb.right(90)
+bawb.forward(35)
+bawb.left(90)
+bawb.pendown()
+bawb.backward(125)
+bawb.begin_fill()
+bawb.forward(250)
+bawb.right(90)
+for i in range(10):
+    bawb.forward(5)
+    bawb.right(5)
+for i in range(8):
+    bawb.forward(10)
+    bawb.right(5)
+bawb.forward(85)
+for i in range(9):
+    bawb.forward(10)
+    bawb.right(5)
+for i in range(10):
+    bawb.forward(5)
+    bawb.right(5)
+bawb.end_fill()
+
+bowb.right(90)
+bowb.forward(35)
+bowb.left(90)
+bowb.forward(90)
+
 bobb.backward(200)
+bbob.forward(100)
 
 turtle.exitonclick()
